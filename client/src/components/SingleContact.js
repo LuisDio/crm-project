aefe63597e6f410003fd3fa7062546d88c0a9833
LuisDio <luis.diolindo@gmail.com>
2020-01-04 16:09:30 +0300

    add Single contact card

--- client/src/components/SingleContact.js
+++ client/src/components/SingleContact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const SingleContact = ({item}) => {
+const SingleContact = ({item}) => (
     <div class="row">
     <div class="col s12 m6">
       <div class="card blue-grey darken-1">
@@ -13,6 +13,6 @@ const SingleContact = ({item}) => {
       </div>
     </div>
   </div>
-}
+)
 
 export default SingleContact;
